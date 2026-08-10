dbc1465337590d4ea01e3d0fb6b4d850c66592d5
felipenalves <felipe@inovadigitalid.com>
2026-08-10 10:27:04 -0300

    feat: logo nova + badges no README; remove app iOS (em desenvolvimento); iPhone via PWA HTTPS (cloudflared); sw v12

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dokke-v11";
+const CACHE = "dokke-v12";
 const PRECACHE = ["/", "/index.html", "/icon-192.png", "/icon-512.png", "/manifest.webmanifest"];
 
 self.addEventListener("install", function(e) {
